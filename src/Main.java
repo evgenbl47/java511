1e01b81603e331cc8bbb7a41f1fd2b73651ac501
nt4h <evgenbl47@gmail.com>
2025-11-04 00:23:55 +0500

    add new line

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+public class Main {
+    public static void main(String[] args) {
+//1
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println();
+//2
+        System.out.println("*");
+        System.out.println(" *");
+        System.out.println("  *");
+        System.out.println("   *");
+        System.out.println("    *");
+        System.out.println("     *");
+        System.out.println("      *");
+        System.out.println("       *");
+        System.out.println();
+//3
+        System.out.println("       *");
+        System.out.println("      *");
+        System.out.println("     *");
+        System.out.println("    *");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*");
+        System.out.println();
+
+//4
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******");
+        System.out.println();
+//5
+        System.out.println("*******");
+        System.out.println("******");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println();
+//6
+        System.out.println("       *");
+        System.out.println("      ***");
+        System.out.println("     *****");
+        System.out.println("    *******");
+        System.out.println("   *********");
+        System.out.println("  ***********");
+        System.out.println(" *************");
+        System.out.println("***************");
+        System.out.println();
+//7
+        System.out.println("        *");
+        System.out.println("       ***");
+        System.out.println("      *****");
+        System.out.println("     *******");
+        System.out.println("    *********");
+        System.out.println("   ***********");
+        System.out.println("  *************");
+        System.out.println(" ***************");
+        System.out.println("*****************");
+        System.out.println(" ***************");
+        System.out.println("  *************");
+        System.out.println("   ***********");
+        System.out.println("    *********");
+        System.out.println("     *******");
+        System.out.println("      *****");
+        System.out.println("       ***");
+        System.out.println("        *");
+
+        System.out.println("end");
+
+
+    }
+}
