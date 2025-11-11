c4a480b872832e8131e6674b9e9d097bb9805736
Windak2011 <Windak2011@yandex.ru>
2025-11-11 14:32:22 +0500

    updade lesson11(7-15) homework

--- Lesson11/src/Main.java
+++ Lesson11/src/Main.java
@@ -4,13 +4,27 @@ public class Main {
 
     public static void run() {
         int[] arr = arrCreate(5, 0, 20);
+        System.out.print("Array: ");
         arrShow(arr);
-        System.out.println();
-        System.out.println("Sum of array: " + arrSum(arr));
-        System.out.println("Amount positive numbers: " +arrShowAmountPositive(arr));
+        System.out.println("\nSum of array: " + arrSum(arr));
+        System.out.println("Amount positive numbers: " + arrShowAmountPositive(arr));
         System.out.println("Max element: " + arrFindMax(arr));
         System.out.println("Min element: " + arrFindMin(arr));
+        System.out.println("Average: " + arrAverage(arr));
+        System.out.println("Even amount: " + arrEven(arr));
+        System.out.println("Max ID: " + arrFindMaxID(arr));
+        System.out.print("Show only positive: ");
+        arrOnlyPositive(arr);
+        System.out.print("\nShow reverse arr: ");
+        arrShow(arrReverse(arr));
+        System.out.println("\nFind equal numbers: " + arrFindByNum(arr, 10));
+        System.out.print("After remove negative: ");
+        arrRemoveNegative(arr);
+        arrShow(arr);
+        System.out.println("\nArrays equal: " + arrSumEqualArr(arrCreate(2, 0, 2), arrCreate(2, 0, 2)));
+        System.out.println("Difference max - min: " + arrDiffMaxMin(arr));
     }
+
 //        Задание 1
 //        Написать метод, который создает массив из N случайных чисел в диапазоне от min до max и возвращает этот массив.
     public static int[] arrCreate(int size, int min, int max) {
@@ -21,7 +35,8 @@ public class Main {
         }
         return arr;
     }
-//        Задание 2
+
+    //        Задание 2
 //        Написать метод, который принимает массив и выводит все его элементы на экран в одну строку через пробел.
     public static void arrShow(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -29,7 +44,7 @@ public class Main {
         }
     }
 
-//        Задание 3
+    //        Задание 3
 //        Написать метод, который принимает массив и возвращает сумму всех его элементов.
     public static int arrSum(int[] arr) {
         int sum = 0;
@@ -52,7 +67,7 @@ public class Main {
     }
 
 
-//        Задание 5
+    //        Задание 5
 //        Написать метод, который принимает массив и возвращает максимальный элемент.
     public static int arrFindMax(int[] arr) {
         int max = arr[0];
@@ -63,46 +78,117 @@ public class Main {
         }
         return max;
     }
-//        Задание 6
+
+    //        Задание 6
 //        Написать метод, который принимает массив и возвращает минимальный элемент.
-public static int arrFindMin(int[] arr) {
-    int min = arr[0];
-    for (int i = 1; i < arr.length; i++) {
-        if (min > arr[i]) {
-            min = arr[i];
+    public static int arrFindMin(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
         }
+        return min;
     }
-    return min;
-}
 
 //        Задание 7
 //        Написать метод, который принимает массив и возвращает среднее арифметическое всех элементов.
+    public static int arrAverage(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            count = count + arr[i];
+        }
+        int result = count / arr.length;
+        return result;
+    }
 
-//        Задание 8
+    //        Задание 8
 //        Написать метод, который принимает массив и возвращает количество четных чисел в нем.
-//
-//        Задание 9
+    public static int arrEven(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //        Задание 9
 //        Написать метод, который принимает массив и возвращает индекс максимального элемента.
-//
-//        Задание 10
+    public static int arrFindMaxID(int[] arr) {
+        int maxID = 0;
+        int maxElement = arrFindMax(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (maxElement == arr[i]) {
+                maxID = i;
+            }
+        }
+        return maxID;
+    }
+
+    //        Задание 10
 //        Написать метод, который принимает массив и выводит только положительные элементы на экран.
-//
-//        Задание 11
+    public static void arrOnlyPositive(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
+    //        Задание 11
 //        Написать метод, который принимает массив и возвращает новый массив в обратном порядке.
-//
-//        Задание 12
+    public static int[] arrReverse(int[] arr) {
+        int[] arrReverse = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arrReverse[arr.length - 1 - i] = arr[i];
+        }
+        return arrReverse;
+    }
+
+    //        Задание 12
 //        Написать метод, который принимает массив и число, возвращает количество элементов равных этому числу.
-//
-//        Задание 13
+    public static int arrFindByNum(int[] arr, int num) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //        Задание 13
 //        Написать метод, который принимает массив и заменяет все отрицательные элементы на нули.
-//
-//        Задание 14
+    public static void arrRemoveNegative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                arr[i] = 0;
+            }
+        }
+    }
+
+    //        Задание 14
 //        Написать метод, который принимает два массива и возвращает true, если их суммы элементов равны, иначе false.
-//
-//        Задание 15
+    public static boolean arrSumEqualArr(int[] first, int[] second) {
+        if (arrSum(first) == arrSum(second)) {
+            return true;
+        }return false;
+    }
+
+    //        Задание 15
 //        Написать метод, который принимает массив и возвращает разницу между максимальным и минимальным элементом.
+    public static int arrDiffMaxMin(int[] arr) {
+        int diff = arrFindMax(arr) - arrFindMin(arr);
+        return diff;
+    }
+
 
     public static void main(String[] args) {
         run();
-        }
     }
+}
