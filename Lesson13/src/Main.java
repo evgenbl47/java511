e41d029a28c89b7bf7a5b66d32c94c6aa778b8b6
evgenbl47 <evgenbl47@gmail.com>
2025-11-25 16:26:01 +0500

    add start game param

--- Lesson13/src/Main.java
+++ Lesson13/src/Main.java
@@ -1,7 +1,60 @@
+import java.util.Random;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int find, min = 0, max = 100, rand;
+        String name;
+
+        System.out.println("Welcome to game ==Guess Number==");
+        System.out.print("Type your name: ");
+        name = scanner.nextLine();
+
+        do {
+            System.out.println("1 - Human-Computer");
+            System.out.println("2 - Computer-Human");
+            System.out.println("3 - Help (off)");
+            System.out.println("4 - Try (off)");
+            System.out.println("5 - Range (0-100");
+            System.out.println("6 -Exit");
+            System.out.println();
+
+            int select = scanner.nextInt();
+
+            if (select == 6) {
+                System.out.println("GoodBye, se you later");
+                break;
+            } else if (select == 1) {
+                System.out.println("Welcome " + name + " to Human-Computer game");
+                System.out.println(name + " guess a number: ");
+                find = scanner.nextInt();
+
+                rand = random.nextInt(max - min + 1) + min;
+
+
+            }
+            if (select == 2) {
+
+            }
+            if (select == 2) {
+
+            }
+            if (select == 2) {
+
+            }
+            if (select == 2) {
+
+            }
+            if (select == 2) {
+
+            }
+
+        } while (true);
 
     }
 }
@@ -50,14 +103,11 @@ public class Main {
         //1 Человек-Компютер
         //Введите имя : Фарид
         //Фарид загадай число : 66
-
         //Компютер угадывает число : 10
-
-        //1 - Больше
-        //2 - Меньше
+            //1 - Больше
+            //2 - Меньше
 
         //Ответ : 1
-
         //Компютер угадывает число : 70
 
         //1 - Больше
@@ -66,7 +116,6 @@ public class Main {
         //Ответ : 2
 
         //Компютер угадывает число : 66
-
         //Урра компютер угадал число с 5 попыток
 
 
