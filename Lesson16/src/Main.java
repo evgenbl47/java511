3694e1be5516ab0cb8d352281d2832f1195287b8
evgenbl47 <evgenbl47@gmail.com>
2025-12-03 16:28:10 +0500

    add lesson16 folder

---
+++ Lesson16/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//20251125_194618
+//try-catch
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+//        try {
+//        } catch (IndexOutOfBoundsException e) {
+//        } catch (ArithmeticException e) {
+//        } catch (NullPointerException e) {
+//        } catch (Exception e) {
+//        } finally {
+//        }
