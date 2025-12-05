46b9e78bc7af51508babb04926f72a7cfe591aa4
evgenbl47 <evgenbl47@gmail.com>
2025-12-05 22:11:42 +0500

    upd lesson16 add, fix some methods

--- Lesson16/src/Main.java
+++ Lesson16/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 //20251125_194618
 //try-catch
 public class Main {
@@ -13,52 +16,169 @@ public class Main {
         return res;
     }
 
+    public static int getElement(int[] arr, int index) {
+        int res = 0;
+
+        try {
+            res = arr[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("");
+            System.out.println("индекс выходит за границы массива");
+            return -1;
+        }
+
+        return res;
+    }
+
+    public static int inputNumber(Scanner scanner) {
+
+        boolean isNumCorrect = false;
+        int number = 0;
+
+        do {
+            System.out.print("Type number                       :");
+            String num = scanner.next();
+            try {
+                number = Integer.parseInt(num);
+                isNumCorrect = true;
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!isNumCorrect);
+
+        return number;
+    }
+
+    public static int parseNumber(String numberStr) {
+        int number = 0;
+        try {
+            number = Integer.parseInt(numberStr);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return number;
+    }
+
+    public static void calculate(double a, double b, char operation) {
+        if (operation == '+') {
+            System.out.print(a + b);
+            return;
+        }
+        if (operation == '-') {
+            System.out.print(a - b);
+            return;
+        }
+        if (operation == '*') {
+            System.out.print(a * b);
+            return;
+        }
+        if (operation == '/') {
+            if (b == 0) {
+                throw new IllegalArgumentException("Devide by 0");
+            }
+            System.out.print(a / b);
+            return;
+        }
+        throw new IllegalArgumentException("Neizvestnaya operaciya");
+    }
+
+    public static boolean validateAge(int age) {
+        if (age >= 0 && age <= 150) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("Age can be >= 0 && <= 150");
+        }
+    }
+
+
+    public static void processArray(int[] arr, int index, int divisor) {
+        int res;
+        try {
+            res = arr[index] / divisor;
+            System.out.println(res);
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
 //        Задача 1: Деление на ноль
 //        Создайте метод safeDivision(int a, int b), который делит два числа и обрабатывает деление на ноль через try-catch.
 //        Метод должен возвращать результат деления или выводить сообщение об ошибке и возвращать 0.
 
         int res = safeDivision(10, 5);
-        System.out.println("Safe division              :" + res);
-
+        System.out.println("safeDivision(int a, int b)        :" + res);
 
 
 //        Задача 2: Проверка массива
-//        Создайте метод getElement(int[] arr, int index), который возвращает элемент массива по индексу. Обработайте ситуацию, когда индекс выходит за границы массива (ArrayIndexOutOfBoundsException). При ошибке возвращайте -1.
-//
+//        Создайте метод getElement(int[] arr, int index), который возвращает элемент массива по индексу.
+//        Обработайте ситуацию, когда индекс выходит за границы массива (ArrayIndexOutOfBoundsException). При ошибке возвращайте -1.
+
+        int elem;
+        int index = 0;
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        elem = getElement(arr, index);
+        System.out.println("getElement(arr, index)            :" + elem);
+
+
 //        Задача 3: Безопасный ввод числа
-//        Создайте метод inputNumber(*Scanner scanner, String message* ),
-//                который просит пользователя ввести целое число.
-//                Если пользователь вводит не число (буквы, символы),
-//        программа должна перехватить исключение,
+//        Создайте метод inputNumber(*Scanner scanner, String message* ), который просит пользователя ввести целое число.
+//        Если пользователь вводит не число (буквы, символы), программа должна перехватить исключение,
 //        вывести сообщение об ошибке и повторить запрос. Метод возвращает корректно введённое число.
-//
-//                Задача 4: Конвертер строки в число
-//        Создайте метод parseNumber(String str), который преобразует строку в целое число. Обработайте NumberFormatException - если строка не является числом, выведите сообщение об ошибке и верните 0. Протестируйте метод со строками: "123", "abc", "12.5".
-//
+        System.out.println("inputNumber()                     :" + inputNumber(scanner));
+
+
+//        Задача 4: Конвертер строки в число
+//        Создайте метод parseNumber(String str), который преобразует строку в целое число.
+//        Обработайте NumberFormatException - если строка не является числом, выведите сообщение об ошибке и верните 0.
+//        Протестируйте метод со строками: "123", "abc", "12.5".
+        System.out.println("parseNumber(123)                  :" + parseNumber("123"));
+
 //                Задача 5: Калькулятор с исключениями
-//        Создайте метод calculate(double a, double b, char operation), который выполняет арифметические операции (+, -, *, /). Обработайте деление на ноль. Если передана неизвестная операция (например, '%'), бросьте исключение IllegalArgumentException с сообщением "Neizvestnaya operaciya".
-//
+//        Создайте метод calculate(double a, double b, char operation), который выполняет арифметические операции (+, -, *, /).
+//        Обработайте деление на ноль. Если передана неизвестная операция (например, '%'), бросьте исключение IllegalArgumentException с сообщением"Neizvestnaya operaciya".
+        System.out.print("calculate(1, 0, /)                :");
+        calculate(1, 0, '/');
+        System.out.println();
+
+
 //                Задача 6: Валидация возраста
-//        Создайте метод validateAge(int age), который проверяет корректность возраста (должен быть от 0 до 150). Если возраст отрицательный или больше 150, бросьте IllegalArgumentException с соответствующим сообщением. В методе main обработайте это исключение и выведите сообщение об ошибке.
-//
-//                Задача 7: Множественные исключения
-//        Создайте метод processArray(int[] arr, int index, int divisor), который берёт элемент массива по индексу и делит его на divisor. Метод должен обрабатывать три типа исключений отдельными catch блоками:
-//
+//        Создайте метод validateAge(int age), который проверяет корректность возраста (должен быть от 0 до 150). Если возраст отрицательный или больше 150,
+//        бросьте IllegalArgumentException с соответствующим сообщением. В методе main обработайте это исключение и выведите сообщение об ошибке.
+        try {
+            System.out.print("validateAge(50)                   :" + validateAge(50));
+        } catch (IllegalArgumentException e) {
+            System.out.println("validateAge(int age)              :" + e.getMessage());
+        }
+        System.out.println();
+
+//        Задача 7: Множественные исключения
+//Создайте метод processArray(int[] arr, int index, int divisor), который берёт элемент массива по индексу и делит его на divisor.
+// Метод должен обрабатывать три типа исключений отдельными catch блоками:
 //        NullPointerException - если массив null
 //        ArrayIndexOutOfBoundsException - если индекс неверный
 //        ArithmeticException - если делитель равен 0
-//
-//        При каждой ошибке выводите своё сообщение. Возвращайте результат деления или -1 при ошибке.
 
+//        При каждой ошибке выводите своё сообщение. Возвращайте результат деления или -1 при ошибке.
+        int[] array = new int[]{2, 3};
+        System.out.println("processArray(array, 50, 0)         :");
+        processArray(array, 555, 5);
+        System.out.println(Arrays.toString(array));
 
 
     }
 }
-
-
-
 
 
 //        try {
