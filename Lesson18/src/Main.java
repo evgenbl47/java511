8813c282b46261cbfcaab6b985a7e7fd0d5a18de
evgenbl47 <evgenbl47@gmail.com>
2025-12-09 00:43:14 +0500

    upd lesson18 add HomeWork T3

--- Lesson18/src/Main.java
+++ Lesson18/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        Person a = new Person("Max", 2);
+
+        System.out.println(a);
+        a.speakHello();
+
+
+
     }
 }
