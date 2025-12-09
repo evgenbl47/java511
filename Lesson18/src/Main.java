961131dd28ab0833424d52f9a21422a2dc406e0e
evgenbl47 <evgenbl47@gmail.com>
2025-12-09 15:39:26 +0500

    upd lesson18 Country

--- Lesson18/src/Main.java
+++ Lesson18/src/Main.java
@@ -38,9 +38,64 @@ class ChildBankAccount {
     }
 }
 
+class Country{
+    private String title;
+    private String cityName;
+    private int population;
+    private double area;
+    private boolean isOceanFront;
+
+    public Country(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        if (population <= 0) {
+            return;
+        }
+        this.population = population;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public boolean isOceanFront() {
+        return isOceanFront;
+    }
+
+    public void setOceanFront(boolean oceanFront) {
+        isOceanFront = oceanFront;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        //1
         ChildBankAccount account =
                 new ChildBankAccount(10000);
         account.depositMoney(1000);
@@ -55,6 +110,12 @@ public class Main {
         account.debitMoney(-50);
         System.out.println("Balance: " + account.getBalance());
 
+
+        //2
+        Country country = new Country("USA");
+        country.setPopulation(1_999_999_999);
+        System.out.println("Country title :" + country.getTitle());
+        System.out.println("Country population :" + country.getPopulation());
 
     }
 }
