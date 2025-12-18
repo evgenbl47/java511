b13d1f75d8520933dd260afde80885571a35ca81
evgenbl47 <evgenbl47@gmail.com>
2025-12-18 14:35:32 +0500

    Commit before merge

--- Lesson20_Abstract_Interface/src/pay/PayPal.java
+++ Lesson20_Abstract_Interface/src/pay/PayPal.java
@@ -3,12 +3,11 @@ package pay;
 public class PayPal implements Payable{
     @Override
     public void processPayment(double amount) {
-
+        System.out.println("Payment to card 1111-2222-3333-4444 is done");
     }
 
     @Override
     public void refoundPayment(double amount) {
-
     }
 
     @Override
