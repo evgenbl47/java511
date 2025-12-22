81fd6333d43aceece76dd25b85a4ab5e3f6f33a8
evgenbl47 <evgenbl47@gmail.com>
2025-12-22 21:38:32 +0500

    lesson 21 add getFirst()

--- Lesson22/src/ClassWork.java
+++ Lesson22/src/ClassWork.java
@@ -1,4 +1,4 @@
-public class MyList {
+public class ClassWork {
 
     int[] arr;
     int size;
@@ -13,62 +13,27 @@ public class MyList {
         for (int i = 0; i < size; i++) {
             tmp[i] = arr[i];
         }
+
         tmp[size] = num;
         size++;
         arr = tmp;
-    }
 
-    public void print() {
-        for (int i = 0; i < size; i++) {
-            System.out.print(arr[i] + " ");
-            ;
-        }
-        System.out.println();
+
+        //num = 4   index = 1
+
+        //  1 2 3
+        //  1 4 2 3
     }
 
     public void addStart(int num) {
         int[] tmp = new int[size + 1];
-        for (int i = size; i > 0; i--) {
-            tmp[i] = arr[i - 1];
-        }
-        tmp[0] = num;
-        arr = tmp;
-        size++;
-    }
-
-    //todo
-    public int getFirst() {
-        int elem;
-        if (size < 0) {
-            System.out.println("First size is less than 0");
-            return elem = 0;
-        }
-        elem = arr[0];
-        return elem;
-    }
-
-    public void removeByValueFirst(int num) {
-        boolean isHas = true;
-
         for (int i = 0; i < size; i++) {
-            if (arr[i] == num) {
-                isHas = true;
-                break;
-            }
+            tmp[i + 1] = arr[i];
         }
 
-        if (isHas) {
-            int[] tmp = new int[size - 1];
-            for (int i = 0, j = 0; i < size; i++) {
-                if (isHas && arr[i] == num) {
-                    isHas = false;
-                    continue;
-                }
-                tmp[j++] = arr[i];
-            }
-            size--;
-            arr = tmp;
-        }
+        tmp[0] = num;
+        size++;
+        arr = tmp;
     }
 
     public void addByIndex(int index, int num) {
@@ -98,7 +63,40 @@ public class MyList {
         }
     }
 
-    public boolean isHasValue(int num) {
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void removeByValueFirst(int num) {
+        boolean isHas = false;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == num) {
+                isHas = true;
+                break;
+            }
+        }
+
+        if (isHas) {
+            int[] tmp = new int[size - 1];
+            for (int i = 0, j = 0; i < size; i++) {
+                if (isHas && arr[i] == num) {
+                    isHas = false;
+                    continue;
+                }
+
+                tmp[j++] = arr[i];
+            }
+
+            size--;
+            arr = tmp;
+        }
+    }
+
+    public  boolean isHasValue(int num) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == num) {
                 return true;
@@ -107,17 +105,20 @@ public class MyList {
 
         return false;
     }
-}
 
+
+
+
+}
 
 //1    +   public  void addEnd(int num);
 //2    +   public  void print();
 //3    +   public  void addStart(int num);
-//4    +   public  int getFirst();
+//4    -   public  int getFirst();
 //5    -   public  boolean isHasValue(int num);
 //9    -   public  int size();
 //10   -   public  void clear();
-//7    +   public  void removeByIndex(int index);
+//7    -   public  void removeByIndex(int index);
 //8    +   public  void removeByValueFirst(int num);
 
 
@@ -129,14 +130,3 @@ public class MyList {
 //15   -    public  int getLast();
 //16   -    public  void addByIndex(int index, int num);
 //17   -    public  void removeByIndexLast(int index);
-
-
-
-
-
-
-
-
-
-
-
---
+++ Lesson22/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ClassWork myList = new ClassWork();
+        myList.addEnd(55);
+        myList.addEnd(66);
+        myList.addEnd(66);
+        myList.addEnd(66);
+        myList.addEnd(77);
+        myList.addEnd(88);
+        myList.addEnd(99);
+        myList.print();
+        myList.addByIndex(3, 999);
+        myList.print();
+
+    }
+}
+
+//
+//        int[] arr = new int[3];
+//        arr[0] = 1;
+//        arr[1] = 2;
+//        arr[2] = 3;
+//
+//        arr = new int[4];
+//        System.out.println(arr[0]);
+
+//        List<Integer> list = new ArrayList<>();
+//
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+//        list.add(6);
+//
+//        for (int i : list) {
+//            System.out.print(i + " ");
+//        }
