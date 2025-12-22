35353299ecb36a64b86799550190fda3dfc8bf29
evgenbl47 <evgenbl47@gmail.com>
2025-12-22 16:28:02 +0500

    lesson21 addStart method

--- Lesson21/src/Main.java
+++ Lesson21/src/Main.java
@@ -2,6 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        MyList list = new MyList();
 
     }
 }
@@ -9,7 +10,7 @@ public class Main {
 //Выполнить от 1 - 9
 //1    +   public  void addEnd(int num);
 //2    +   public  void print();
-//3    -   public  void addStart(int num);
+//3    +   public  void addStart(int num);
 //4    -   public  int getFirst();
 //5    -   public  boolean isHasValue(int num);
 //6    -   public  int size();
@@ -17,59 +18,3 @@ public class Main {
 //8    -   public  void removeByIndex(int index);
 //9    +   public  void removeByValueFirst(int num);
 
-
-
-class MyListt {
-    int[] arr;
-    int size;
-
-    {
-        size = 0;
-        arr = new int[size];
-    }
-
-    public void addEnd(int num) {
-        int[] tmp = new int[size + 1];
-        for (int i = 0; i < size; i++) {
-            tmp[i] = arr[i];
-        }
-
-        tmp[size] = num;
-        size++;
-        arr = tmp;
-
-    }
-
-    public void print() {
-        for (int i = 0; i < size; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public  void removeByValueFirst(int num){
-        boolean isHas = false;
-
-        for (int i = 0; i < size; i++) {
-            if (arr[i] == num) {
-                isHas = true;
-                break;
-            }
-        }
-
-        if (isHas) {
-            int[] tmp = new int[size - 1];
-            for (int i = 0,j=0; i < size; i++) {
-                if (isHas && arr[i] == num) {
-                    isHas = false;
-                    continue;
-                }
-
-                tmp[j++] = arr[i];
-            }
-
-            size--;
-            arr = tmp;
-        }
-    }
-}
--- Lesson21/src/MyList.java
+++ Lesson21/src/MyList.java
@@ -13,22 +13,66 @@ public class MyList {
         for (int i = 0; i < size; i++) {
             tmp[i] = arr[i];
         }
+        tmp[size] = num;
+        size++;
+        arr = tmp;
     }
 
     public void print() {
         for (int i = 0; i < size; i++) {
             System.out.print(arr[i] + " ");;
-            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void removeByValueFirst(int num) {
+        boolean isHas = true;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == num) {
+                isHas = true;
+                break;
+            }
+        }
+
+        if (isHas) {
+            int[] tmp = new int[size - 1];
+            for (int i = 0, j = 0; i < size; i++) {
+                if (isHas && arr[i] == num) {
+                    isHas = false;
+                    continue;
+                }
+                tmp[j++] = arr[i];
+            }
+            size--;
+            arr = tmp;
         }
     }
 
-    //todo correct this method. Remove firs find element
-    public void removeByValueFirst(int num) {
-        int[] tmp = new int[size - 1];
-        for (int i = 0; i < tmp.length; i++) {
-            tmp[i] = arr[i + 1];
+    public void addStart(int num) {
+        int[] tmp = new int[size + 1];
+        for (int i = size; i > 0; i--) {
+//            System.out.printf("size = %d, i = %d\n", size, i);
+//            System.out.printf("arr[%d] = %d\n", i, arr[i- 1]);
+//            System.out.printf("tmp[%d], arr[%d]\n", tmp[i], arr[i - 1]);
+            tmp[i] = arr[i - 1];
         }
+        tmp[0] = num;
         arr = tmp;
-        size--;
+        size++;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
