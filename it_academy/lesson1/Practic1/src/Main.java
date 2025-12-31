b3fd1fb7a47c3f6842a1722e14e106953cfdcc99
evgenbl47 <evgenbl47@gmail.com>
2026-01-01 03:33:31 +0500

    add task for debug

--- it_academy/lesson1/Practic1/src/Main.java
+++ it_academy/lesson1/Practic1/src/Main.java
@@ -1,7 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-    }
-}
+                Scanner scanner = new Scanner(System.in);
+                // Просим пользователя ввести целое число больше 1
+                System.out.print("Введите целое число больше 1: ");
+                int n = scanner.nextInt();
+
+                // Проверяем, является ли число простым
+                if(n <= 1){
+                    return;
+                }
+
+                boolean hasDivisor = false;
+
+
+                // Цикл для поиска первого делителя числа n
+                for (int i = 2; i < n; i++) {
+                    if (n % i == 0) { // Если n делится на i без остатка
+                        // Выводим первый найденный делитель
+                        System.out.println(i);
+                        // Устанавливаем флаг, что число не простое
+                        hasDivisor = true;
+                        // Завершаем цикл, так как делитель найден
+                        break;
+                    }
+                }
+
+                // Если делитель не найден, выводим, что число простое
+                if (!hasDivisor) {
+                    System.out.println("Число простое");
+                }
+            }
+        }
