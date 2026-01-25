588c3da423222ce36a9347893bcba20b7a19af48
evgenbl47 <evgenbl47@gmail.com>
2026-01-25 22:40:27 +0500

    Move lesson25 to lesson 24

--- Lesson25/src/Main.java
+++ Lesson25/src/Main.java
@@ -1,13 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        SingleLinkedList linkedList = new SingleLinkedList();
 
-        linkedList.add("list1");
-        linkedList.add("list2");
-        linkedList.addStart("list0");
-        linkedList.add(1, "list1.1");
-        linkedList.add(9, "end");
-        linkedList.print();
     }
 }
