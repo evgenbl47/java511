7e7ad4948d2aa092776bde8a91d29671980b3283
evgenbl47 <evgenbl47@gmail.com>
2026-01-30 15:18:46 +0500

    lesson28

---
+++ Lesson28 File/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File folder = new File("folder"); //create folder
+        folder.mkdir();
+
+        File dir = new File("folder/a/b/c/d/e"); //create dirs
+        dir.mkdirs();
+
+        File file = new File("file.txt"); //create file txt, png ...
+        file.createNewFile();
+
+
+    }
+}
