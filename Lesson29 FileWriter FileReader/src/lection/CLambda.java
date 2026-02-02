696c5662658efae4668aed6e9f7329d803adec90
evgenbl47 <evgenbl47@gmail.com>
2026-02-02 16:26:21 +0500

    lesson29 lambda

---
+++ Lesson29 FileWriter FileReader/src/lection/CLambda.java
@@ -0,0 +1,53 @@
+package lection;
+
+@FunctionalInterface
+interface Testable {
+    //    void test();
+    int operation(int a, int b);
+}
+
+interface Checkable {
+    boolean check(int a);
+}
+
+public class CLambda {
+    void run() {
+//        Testable addition = (a, b) -> a + b;
+//        Testable subtract = (a, b) -> a - b;
+//        Testable divide   = (a, b) -> a / b;
+//        Testable multyply = (a, b) -> a * b;
+
+//        Testable other = (a, b) -> {
+//            a -= b;
+//            a += b;
+//            return b;
+//        };
+
+//        divide.operation(20, 10);
+//        addition.operation(1, 2);
+
+//        System.out.println("hello");
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        print(arr, (x) -> true);
+        print(arr, (x) -> x % 2 == 0);
+        print(arr, (x) -> x % 2 != 0);
+        print(arr, (x) -> x > 5);
+        print(arr, (x) -> x < 5);
+        print(arr, (x) -> x == 5);
+        print(arr, (x) -> x == 5);
+    }
+
+    public void print(int[] arr, Checkable checkable) {
+        for (int i : arr) {
+            if (checkable.check(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+
+}
+
+
