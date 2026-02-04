816134d9272c6b67867c36b0505fe9e1d732a9f4
evgenbl47 <evgenbl47@gmail.com>
2026-02-05 02:18:10 +0500

    lesson30 contunue StreamApi

--- Lesson30 StreamAPI/src/Main.java
+++ Lesson30 StreamAPI/src/Main.java
@@ -19,18 +19,22 @@ public class Main {
 
         Stream<Object> stream = Stream.builder().add("a1").add("a2").add("a3").build();
 //        stream.forEach(System.out::println);
-        stream.forEach( x -> {
-            System.out.println(x);
-        });
+        stream.forEach( x -> System.out.println(x));
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(i);
-        }
+
+
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            list.add(i);
+//        }
 
         //filter
-        list = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+//        list = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+//        list = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
 
+
+        List<String> list = Arrays.asList("one", "two", "three", "four");
+        list.stream().flatMap(word -> Arrays.stream(word.split(""))).forEach(System.out::println);
 
         class Person{
             String name;
@@ -51,15 +55,37 @@ public class Main {
         }
 
         List<Person> people = new ArrayList<>();
+        people = people.stream().filter(x -> {
+//            String res = x.name.substring(4);
+//            Integer num = Integer.parseInt(res);
+            Integer num = Integer.parseInt(x.name.substring(4));
+
+//            if (num > 18) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+            return (num > 18);
+        }).collect(Collectors.toList());
+
+        people = people.stream().sorted((x, y) -> x.name.compareTo(y.name)).collect(Collectors.toList());
+
+        int[] age = people.stream().mapToInt(x -> x.age).toArray();
+
         for (int i = 15; i < 22; i++) {
             people.add(new Person("name" + i, i));
 
         }
         people.add(new Person("Maxim" + 18, 19));
 
+        people.stream()
+                .peek(x -> System.out.println("hello: " + x.name))
+                .filter(x -> x.age > 19)
+                .peek(x -> System.out.println("my age: " + x.age))
+                .forEach(System.out::println);
 
 //        people = people.stream().filter(x -> x.age > 18).collect(Collectors.toList());
-        people = people.stream().filter(x -> x.name.contains("Max")).collect(Collectors.toList());
+//        people = people.stream().filter(x -> x.name.contains("Max")).collect(Collectors.toList());
 
         for (int i = 0; i < people.size(); i++) {
             System.out.println(people.get(i) + " ");
