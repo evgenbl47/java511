4e5af1191e268a2df358955573002fa235b48e3d
evgenbl47 <evgenbl47@gmail.com>
2026-02-04 16:28:24 +0500

    lesson30 StreamApi classWork

--- Lesson30 StreamAPI/src/Main.java
+++ Lesson30 StreamAPI/src/Main.java
@@ -18,7 +18,10 @@ public class Main {
 //        Stream<String> stream = list.stream();
 
         Stream<Object> stream = Stream.builder().add("a1").add("a2").add("a3").build();
-        stream.forEach(System.out::println);
+//        stream.forEach(System.out::println);
+        stream.forEach( x -> {
+            System.out.println(x);
+        });
 
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -28,5 +31,38 @@ public class Main {
         //filter
         list = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
 
+
+        class Person{
+            String name;
+            int age;
+
+            public Person(String name, int age) {
+                this.name = name;
+                this.age = age;
+            }
+
+            @Override
+            public String toString() {
+                return "Person{" +
+                        "name='" + name + '\'' +
+                        ", age=" + age +
+                        '}';
+            }
+        }
+
+        List<Person> people = new ArrayList<>();
+        for (int i = 15; i < 22; i++) {
+            people.add(new Person("name" + i, i));
+
+        }
+        people.add(new Person("Maxim" + 18, 19));
+
+
+//        people = people.stream().filter(x -> x.age > 18).collect(Collectors.toList());
+        people = people.stream().filter(x -> x.name.contains("Max")).collect(Collectors.toList());
+
+        for (int i = 0; i < people.size(); i++) {
+            System.out.println(people.get(i) + " ");
+        }
     }
 }
