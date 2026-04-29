dccf0caf301dddcd038869860e537c0bc270baae
evgenbl47 <evgenbl47@gmail.com>
2026-04-29 23:27:46 +0500

    basic actions lesson2

---
+++ javaScript/2-object/script.js
@@ -0,0 +1,23 @@
+let person ={
+   surname: "Nt4h",
+
+   age: 30,
+   
+   hi: function(){
+      console.log("hello");
+   },
+   arr: ["", "", ""],
+   dogs: [
+      {
+         "Martin"
+      },
+      {
+         "Nota"
+      },
+      {
+         "Archi"
+      }
+   ]
+}
+
+console.log(person.hi);
