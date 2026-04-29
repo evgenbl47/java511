565bc3f1e84f543a21a04ce53e66d9a91be8af86
evgenbl47 <evgenbl47@gmail.com>
2026-04-29 15:51:02 +0500

    base actions

---
+++ javaScript/1-intro/script.js
@@ -0,0 +1,35 @@
+// console.log("Hello World");
+
+
+
+// var x;
+// var x = 10;
+// x = 'f';
+// x = 4.5;
+// x = true;
+// x = [1, 2, 3, 4];
+// x = null;
+// x = {};
+
+// console.log(x);
+
+
+
+// console.log("Value: " + x);
+// console.log("Type: " + typeof(x));
+
+
+
+// var x = 10 / 0; //infinity
+// var x; //undefind;
+// var x = "Something" / 2; //NaN
+
+// number - любое целое число и дробное число, infinity, NaN
+// undefined - неинициализированная переменная
+// string - любое строковое значение
+// object - array, object, null
+// boolean - true, false
+
+
+
+var x = parseInt(x);
