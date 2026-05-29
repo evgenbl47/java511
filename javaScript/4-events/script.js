6256c79242ddd3e85556491dd426acfc818bf268
evgenbl47 <evgenbl47@gmail.com>
2026-05-29 14:57:03 +0500

    js lesson4  events input field

--- javaScript/4-events/script.js
+++ javaScript/4-events/script.js
@@ -5,6 +5,10 @@ function test(msg) {
 
 let h2 = document.querySelector('h2');
 
+// показать / скрыть блок
+// let span = document.querySelector('span');
+// span.style.display = 'block'/'none'
+
 // h2.onclick = test;
 
 //через анонимную функцию
@@ -55,8 +59,48 @@ h2.addEventListener('click', function(){
 // h2.addEventListener('mouseleave',() =>{
 
     // срабатывае при прокручивании колесика
-h2.addEventListener(' wheel',() =>{
+h2.addEventListener('wheel',() =>{
      console.log("Try to understand");
 });
 
+
+
+let input = document.querySelector('input');
+//в скобках можно написать параметр и получить его состояние
+////срабатывает при нажатии клавиши
+// input.addEventListener('keydown', ()=>{
+//     console.log(event); //выводит информацию о состоянии клавиатуры, какие клавиши были нажаты
+
+//срабатывает при отпускании клавиши
+// input.addEventListener('keyup', ()=>{  
+
+// input.addEventListener('keyup', ()=>{  
+//         console.log(event.key); //печатает нажатую клавишу
+//         console.log(input.value); //весть текст в поле
+        
+// })
+
+//если нажата клавиша f отменяет дефолтное поведение - не дает ввести клавишу
+// input.addEventListener('keydown', ()=>{
+//     if(event.key == f){
+//         event.preventDefault();
+//     }
+// })
+
+
+//проверяет число ли это
+function isNumeric(num) {
+    for(let i = 0; i < 10; i++){
+        if(num == i) return true;
+    }
+
+    return false;
+}
+
+//если в поле попытаться ввести число то ничего не получится
+input.addEventListener('keydown', ()=>{  //добавляет евент слушатель
+    if (isNumeric(event.key)) { //проверяет - введенный символ число?
+        event.preventDefault(); //если число - то отменяет дефолтное поведение 
+    }
+})
 //40:00
