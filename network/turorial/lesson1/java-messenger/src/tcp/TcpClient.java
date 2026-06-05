85867fbda5073818ef904f7ad91dcc7caf4fc9e3
evgenbl47 <evgenbl47@gmail.com>
2026-06-05 15:58:05 +0500

    Create TcpClient.java, and Socket

---
+++ network/turorial/lesson1/java-messenger/src/tcp/TcpClient.java
@@ -0,0 +1,13 @@
+package tcp;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class TcpClient {
+    public static void main(String[] args) throws IOException {
+        String message = "Hello, World!";
+
+        Socket socket = new Socket("127.0.0.1", 27015);
+
+    }
+}
