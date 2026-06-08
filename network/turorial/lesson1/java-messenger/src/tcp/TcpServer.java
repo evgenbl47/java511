b8dde5c0e7777626cc8cf3fd16953e2b1093e0b9
evgenbl47 <evgenbl47@gmail.com>
2026-06-08 16:09:13 +0500

    Add ServerSocket in TcpServer.java

--- network/turorial/lesson1/java-messenger/src/tcp/TcpServer.java
+++ network/turorial/lesson1/java-messenger/src/tcp/TcpServer.java
@@ -1,7 +1,11 @@
 package tcp;
 
-public class TcpServer {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class TcpServer {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(27015);
     }
 }
